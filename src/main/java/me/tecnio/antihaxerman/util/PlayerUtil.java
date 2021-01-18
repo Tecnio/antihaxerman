e09e9718ebcdba0806be10432a90537bbc36ea2a
Nik <61439310+NikV2@users.noreply.github.com>
2021-01-18 23:07:06 +0200

    Faster method for getting nearby entities.
    We'll now check for all vehicles instead of just boats.

--- src/main/java/me/tecnio/antihaxerman/util/PlayerUtil.java
+++ src/main/java/me/tecnio/antihaxerman/util/PlayerUtil.java
@@ -20,10 +20,17 @@ package me.tecnio.antihaxerman.util;
 import io.github.retrooper.packetevents.PacketEvents;
 import io.github.retrooper.packetevents.utils.player.ClientVersion;
 import lombok.experimental.UtilityClass;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @UtilityClass
 public class PlayerUtil {
@@ -53,6 +60,45 @@ public class PlayerUtil {
 
     public double getBaseGroundSpeed(final Player player) {
         return 0.289 + (getPotionLevel(player, PotionEffectType.SPEED) * 0.062f) + ((player.getWalkSpeed() - 0.2f) * 1.6f);
+    }
+
+    /**
+     * Bukkit's getNearbyEntities method looks for all entities in all chunks
+     * This is a lighter method and can also be used Asynchronously since we won't load any chunks
+     *
+     * @param location The location to scan for nearby entities
+     * @param radius   The radius to expand
+     * @return The entities within that radius
+     * @author Nik
+     */
+    public List<Entity> getEntitiesWithinRadius(final Location location, final double radius) {
+
+        final double expander = 16.0D;
+
+        final double x = location.getX();
+        final double z = location.getZ();
+
+        final int minX = (int) Math.floor((x - radius) / expander);
+        final int maxX = (int) Math.floor((x + radius) / expander);
+
+        final int minZ = (int) Math.floor((z - radius) / expander);
+        final int maxZ = (int) Math.floor((z + radius) / expander);
+
+        final World world = location.getWorld();
+
+        List<Entity> entities = new ArrayList<>();
+
+        for (int xVal = minX; xVal <= maxX; xVal++) {
+            for (int zVal = minZ; zVal <= maxZ; zVal++) {
+                if (world.isChunkLoaded(xVal, zVal)) {
+                    entities.addAll(Arrays.asList(world.getChunkAt(xVal, zVal).getEntities()));
+                }
+            }
+        }
+
+        entities.removeIf(entity -> entity.getLocation().distanceSquared(location) > radius * radius);
+
+        return entities;
     }
 
     public int getPotionLevel(final Player player, final PotionEffectType effect) {
