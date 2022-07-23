2b143a398075a0c79fe19c87006cef9e91f9a5ff
visualbasic <89816944+Invisibleeeee@users.noreply.github.com>
2022-07-22 20:39:36 -0700

    Remove
    Check is not really needed

--- src/main/java/me/tecnio/antihaxerman/check/impl/player/timer/TimerB.java
+++ src/main/java/me/tecnio/antihaxerman/check/impl/player/timer/TimerB.java
@@ -22,14 +22,9 @@ import me.tecnio.antihaxerman.check.api.CheckInfo;
 import me.tecnio.antihaxerman.data.PlayerData;
 import me.tecnio.antihaxerman.exempt.type.ExemptType;
 import me.tecnio.antihaxerman.packet.Packet;
-import me.tecnio.antihaxerman.util.MathUtil;
-import me.tecnio.antihaxerman.util.type.EvictingList;
 
 @CheckInfo(name = "Timer", type = "B", description = "Checks packet delay between packets.", experimental = true)
 public final class TimerB extends Check {
-
-    private final EvictingList<Long> samples = new EvictingList<>(50);
-    private long lastFlying;
 
     public TimerB(final PlayerData data) {
         super(data);
@@ -37,42 +32,5 @@ public final class TimerB extends Check {
 
     @Override
     public void handle(final Packet packet) {
-        if (packet.isFlying()) {
-            final long now = now();
-
-            final boolean exempt = this.isExempt(ExemptType.TPS, ExemptType.TELEPORT, ExemptType.JOINED, ExemptType.VEHICLE);
-
-            handle: {
-                if (exempt) break handle;
-
-                final long delay = now - lastFlying;
-
-                if (delay > 0) {
-                    samples.add(delay);
-                }
-
-                if (samples.isFull()) {
-                    final double average = MathUtil.getAverage(samples);
-                    final double deviation = MathUtil.getStandardDeviation(samples);
-
-                    final double speed = 50.0 / average;
-
-                    final boolean invalid = deviation < 40.0 && speed < 0.6 && !Double.isNaN(deviation);
-
-                    if (invalid) {
-                        if (increaseBuffer() > 30) {
-                            fail();
-                            multiplyBuffer(0.50);
-                        }
-                    } else {
-                        decreaseBufferBy(10);
-                    }
-                }
-            }
-
-            this.lastFlying = now;
-        } else if (packet.isTeleport()) {
-            samples.add(125L);
-        }
     }
 }
