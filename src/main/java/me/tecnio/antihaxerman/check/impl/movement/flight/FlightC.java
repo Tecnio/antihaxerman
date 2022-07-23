da68013ab3d3ba88f818ab6f6bbff99c9b630503
visualbasic <89816944+Invisibleeeee@users.noreply.github.com>
2022-07-22 20:25:51 -0700

    Removed
    This does not look like a very stable check. It seems it would false chunk not loading after teleport?

--- src/main/java/me/tecnio/antihaxerman/check/impl/movement/flight/FlightC.java
+++ src/main/java/me/tecnio/antihaxerman/check/impl/movement/flight/FlightC.java
@@ -23,7 +23,7 @@ import me.tecnio.antihaxerman.data.PlayerData;
 import me.tecnio.antihaxerman.exempt.type.ExemptType;
 import me.tecnio.antihaxerman.packet.Packet;
 
-@CheckInfo(name = "Flight", type = "C", description = "Checks for small vertical movement.")
+@CheckInfo(name = "Flight", type = "C", description = "")
 public final class FlightC extends Check {
     public FlightC(final PlayerData data) {
         super(data);
@@ -32,25 +32,6 @@ public final class FlightC extends Check {
     @Override
     public void handle(final Packet packet) {
         if (packet.isFlying()) {
-            final boolean onGround = !data.getPositionProcessor().isInAir() || data.getPositionProcessor().isOnGround();
-
-            final double deltaY = data.getPositionProcessor().getDeltaY();
-            final double lastDeltaY = data.getPositionProcessor().getLastDeltaY();
-
-            final double difference = Math.abs(deltaY - lastDeltaY);
-
-            final boolean exempt = isExempt(ExemptType.VELOCITY, ExemptType.PISTON, ExemptType.VEHICLE, ExemptType.TELEPORT,
-                    ExemptType.LIQUID, ExemptType.BOAT, ExemptType.FLYING, ExemptType.WEB, ExemptType.SLIME, ExemptType.VOID,
-                    ExemptType.CLIMBABLE, ExemptType.CHUNK) || Math.abs(deltaY) > 3.0 || Math.abs(lastDeltaY) > 3.0;
-            final boolean invalid = difference < 0.01 && !onGround;
-
-            if (invalid && !exempt) {
-                if (increaseBuffer() > 4) {
-                    fail();
-                }
-            } else {
-                decreaseBufferBy(0.25);
-            }
         }
     }
 }
