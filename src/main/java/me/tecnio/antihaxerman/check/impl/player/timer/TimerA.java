0495d4de5289062ca8160525f5933d931b19b105
visualbasic <89816944+Invisibleeeee@users.noreply.github.com>
2022-07-22 20:53:41 -0700

    Update TimerA.java
    This code isn't eyecandy but it should work. I left some comments if you want to see my thought process, lol.

--- src/main/java/me/tecnio/antihaxerman/check/impl/player/timer/TimerA.java
+++ src/main/java/me/tecnio/antihaxerman/check/impl/player/timer/TimerA.java
@@ -24,15 +24,18 @@ import me.tecnio.antihaxerman.check.api.CheckInfo;
 import me.tecnio.antihaxerman.data.PlayerData;
 import me.tecnio.antihaxerman.exempt.type.ExemptType;
 import me.tecnio.antihaxerman.packet.Packet;
-import me.tecnio.antihaxerman.util.MovingStats;
+import me.tecnio.antihaxerman.util.type.EvictingList;
+import me.tecnio.antihaxerman.util.MathUtil;
 
-@CheckInfo(name = "Timer", type = "A", description = "Detects game speed modifications.")
+@CheckInfo(name = "Timer", type = "A", description = "Detects slow timer via sampling and averaging.")
 public final class TimerA extends Check {
 
-    private final MovingStats movingStats = new MovingStats(20);
+    private final EvictingList<Long> largesamples = new EvictingList<>(100);
+    //five seconds worth of flyings.
+    private final EvictingList<Long> samples = new EvictingList<>(20);
+    //one second worth of flyings.
 
     private long lastFlying = 0L;
-    private long allowance = 0;
 
     public TimerA(final PlayerData data) {
         super(data);
@@ -42,32 +45,37 @@ public final class TimerA extends Check {
     public void handle(final Packet packet) {
         if (packet.isFlying()) {
             final long now = now();
-
-            final boolean exempt = this.isExempt(ExemptType.TPS, ExemptType.TELEPORT, ExemptType.JOINED, ExemptType.VEHICLE, ExemptType.LAGGING, ExemptType.PING);
-
-            handle: {
-                if (exempt) break handle;
-
-                final long delay = now - lastFlying;
-                if (delay < 10) break handle;
-
-                movingStats.add(delay);
-
-                final double threshold = 7.07;
-                final double deviation = movingStats.getStdDev(threshold);
-
-                if (deviation < threshold && !Double.isNaN(deviation)) {
-                    allowance += 50 - delay;
-
-                    if (allowance > Math.ceil(threshold)) fail();
-                } else {
-                    allowance = 0;
+            final long delta = now - lastFlying;
+            if(lastFlying != 0 && !this.isExempt(ExemptType.TPS, ExemptType.TELEPORT, ExemptType.JOINED, ExemptType.VEHICLE)) {
+                //Make sure delta is correct make sure not exempt and conditions to sample are good
+                samples.add(delta);
+                largesamples.add(delta);
+                if(largesamples.isFull()) {
+                    //make sure samples are full
+                    double speed = 50 / MathUtil.getAverage(samples);
+                    double reliablespeed = 50 / MathUtil.getAverage(largesamples);
+                    //Calculates two different speeds with different sample sizes
+                    double drift = Math.abs(speed - reliablespeed);
+                    //Calculates change of speed over 5 seconds.
+                    
+                    double std = MathUtil.getStandardDeviation(reliablespeed);
+                    double std2 = MathUtil.getStandardDeviation(speed);
+                    //Checks how accurate both speed estimates are.
+                    
+                    if(reliablespeed <= 0.95 && speed <= 0.95 && (std <= 64.0 && std2 <= 42.0 && drift <= 0.01 || std <= 350.0 && std2 <= 64.0 && drift <= 0.001)) {
+                        //If we have somewhat high confidence of both speeds, and the drift is low and both speeds are < 95%, flag.
+                        if(increaseBuffer() > 3) {
+                            //Just incase this condition is met for some reason by a legit. (which should be rare).
+                            fail("Drift: " + drift + " Standard Dev: " + std + " STD2: " + std2 + " Speed: " + reliablespeed);
+                            //Some debug info
+                            setBuffer(2.5);
+                        }
+                    } else {
+                        this.decreaseBufferBy(0.025);
+                    }
                 }
             }
-
-            this.lastFlying = now;
-        } else if (packet.isTeleport()) {
-            movingStats.add(125L);
+            lastFlying = now;
         }
     }
 }
